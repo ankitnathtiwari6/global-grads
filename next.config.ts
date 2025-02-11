d81045eb58fc2404039a71b75b6655dbb74935c7
ankitnathtiwari6 <ankitnathtiwari6@gmail.com>
2025-02-11 13:21:06 +0530

    fixed 3

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/global-grads", // Your repository name
-  assetPrefix: "https://ankitnathtiwari6.github.io/language-academy",
+  assetPrefix: "https://ankitnathtiwari6.github.io/global-grad",
   images: {
     unoptimized: true,
   },
 };
 
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   basePath: "", // Your repository name
+//   images: {
+//     unoptimized: true,
+//   },
+// };
 export default nextConfig;
