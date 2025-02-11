75bd070d3f1eac62c5ea6c200fefdcac71dd3971
ankitnathtiwari6 <ankitnathtiwari6@gmail.com>
2025-02-11 14:01:33 +0530

    fixed asset path

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/global-grads", // Your repository name
-  assetPrefix: "https://ankitnathtiwari6.github.io/global-grads/",
+  assetPrefix: "https://ankitnathtiwari6.github.io",
   images: {
     unoptimized: true,
   },
 };
-
-// const nextConfig: NextConfig = {
-//   output: "export",
-//   basePath: "", // Your repository name
-//   images: {
-//     unoptimized: true,
-//   },
-// };
 export default nextConfig;
