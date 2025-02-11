01b094214c5b37adb6b83365028a0e2bd4f8e3f5
ankitnathtiwari6 <ankitnathtiwari6@gmail.com>
2025-02-11 13:07:40 +0530

    fixed 2

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: "/global-grads/", // Should match basePath with trailing slash
   basePath: "/global-grads", // Your repository name
-
+  assetPrefix: "https://ankitnathtiwari6.github.io/language-academy",
   images: {
     unoptimized: true,
   },
