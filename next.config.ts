eb56925f123568898579019b5cc49219f45fe096
ankitnathtiwari6 <ankitnathtiwari6@gmail.com>
2025-02-11 12:42:10 +0530

    commit 6

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/global-grads",
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
